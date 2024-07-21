11e3f45c4b4537103b2f2d5c31714a38f4b0b17c
ErikaNunes <erikanunes0512@gmail.com>
2024-07-21 20:58:24 -0300

    test: simulando login com o service

--- tests/unit/LoginForm.spec.ts
+++ tests/unit/LoginForm.spec.ts
@@ -2,6 +2,9 @@ import { mount } from "@vue/test-utils";
 import LoginForm from "@/modules/login/components/LoginForm.vue";
 import { createPinia, setActivePinia } from "pinia";
 import { useLoginStore } from "@/modules/login/store";
+import { LoginService } from "@/modules/login/services/loginService";
+
+jest.mock("@/modules/login/services/loginService");
 
 describe("LoginForm.vue", () => {
   let loginStore: ReturnType<typeof useLoginStore>;
@@ -29,5 +32,20 @@ describe("LoginForm.vue", () => {
     const password = wrapper.find("input#password");
     await password.setValue("123456");
     expect(loginStore.payloadLogin.password).toBe("123456");
+  });
+
+  it("call login service on form", async () => {
+    (LoginService.prototype.signIn as jest.Mock).mockResolvedValue({
+      token: "bhvvuhuhvg",
+    });
+
+    const wrapper = mount(LoginForm);
+    await wrapper.find("input#email").setValue("eve.holt@reqres.in");
+    await wrapper.find("input#password").setValue("123456");
+    await wrapper.find("form").trigger("submit.prevent");
+    expect(LoginService.prototype.signIn).toHaveBeenCalledWith({
+      email: "eve.holt@reqres.in",
+      password: "123456",
+    });
   });
 });
