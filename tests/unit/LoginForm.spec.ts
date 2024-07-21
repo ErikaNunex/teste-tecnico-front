606450267415a807d471fd5042011598f723ded2
ErikaNunes <erikanunes0512@gmail.com>
2024-07-21 20:51:39 -0300

    test: testando renderização do componente

---
+++ tests/unit/LoginForm.spec.ts
@@ -0,0 +1,22 @@
+import { mount } from "@vue/test-utils";
+import LoginForm from "@/modules/login/components/LoginForm.vue";
+import { createPinia, setActivePinia } from "pinia";
+import { useLoginStore } from "@/modules/login/store";
+
+describe("LoginForm.vue", () => {
+  let loginStore: ReturnType<typeof useLoginStore>;
+
+  beforeEach(() => {
+    setActivePinia(createPinia());
+    loginStore = useLoginStore();
+  });
+
+  it("render login form", () => {
+    const wrapper = mount(LoginForm);
+    expect(wrapper.find("label[for='email']").text()).toBe("Email");
+    expect(wrapper.find("input#email").exists()).toBe(true);
+    expect(wrapper.find("label[for='password']").text()).toBe("Senha");
+    expect(wrapper.find("input#password").exists()).toBe(true);
+    expect(wrapper.find("button").text()).toBe("Entrar");
+  });
+});
