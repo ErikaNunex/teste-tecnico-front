45f62c61d03717c06effa7804bca3f80873d1684
ErikaNunes <erikanunes0512@gmail.com>
2024-07-21 15:25:43 -0300

    fix: separando componente login da view

---
+++ src/modules/login/components/LoginForm.vue
@@ -0,0 +1,37 @@
+<template>
+  <form @submit.prevent="submit">
+    <div class="form-group">
+      <label for="username">Usuário</label>
+      <input
+        type="text"
+        class="form-control"
+        id="username"
+        v-model="loginStore.payloadLogin.username"
+        placeholder="Digite seu usuário"
+      />
+    </div>
+    <div class="form-group">
+      <label for="password">Senha</label>
+      <input
+        type="password"
+        class="form-control"
+        id="password"
+        v-model="loginStore.payloadLogin.password"
+        placeholder="Digite sua senha"
+      />
+    </div>
+    <div class="card-body">
+      <button type="submit" class="btn btn-primary btn-block">Entrar</button>
+    </div>
+  </form>
+</template>
+<script setup lang="ts">
+import { ref } from "vue";
+import { LoginStore } from "@/modules/login/store";
+const loginStore = LoginStore();
+
+const submit = () => {
+  console.log("Usuário:", loginStore.payloadLogin.username);
+  console.log("Senha:", loginStore.payloadLogin.password);
+};
+</script>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -3,48 +3,14 @@
     <div class="card">
       <div class="card-body">
         <h2 class="card-title text-center">Login</h2>
-        <form @submit.prevent="submit">
-          <div class="form-group">
-            <label for="username">Usuário</label>
-            <input
-              type="text"
-              class="form-control"
-              id="username"
-              v-model="username"
-              placeholder="Digite seu usuário"
-            />
-          </div>
-          <div class="form-group">
-            <label for="password">Senha</label>
-            <input
-              type="password"
-              class="form-control"
-              id="password"
-              v-model="password"
-              placeholder="Digite sua senha"
-            />
-          </div>
-          <div class="card-body">
-            <button type="submit" class="btn btn-primary btn-block">
-              Entrar
-            </button>
-          </div>
-        </form>
+        <LoginForm />
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
-
-const username = ref("");
-const password = ref("");
-
-const submit = () => {
-  console.log("Usuário:", username.value);
-  console.log("Senha:", password.value);
-};
+import LoginForm from "@/modules/login/components/LoginForm.vue";
 </script>
 
 <style scoped>
