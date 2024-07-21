331d1b8d2c4dbccba771ead5431027d9d67c6992
ErikaNunes <erikanunes0512@gmail.com>
2024-07-21 18:46:38 -0300

    feat: craindo repositório para login

---
+++ src/repositories/LoginRepository.ts
@@ -0,0 +1,12 @@
+import { post } from "@/repositories/BaseRepository";
+import { LoginPayloadInterface } from "@/modules/login/interfaces/LoginPayloadInterface";
+import { LoginResponseInterface } from "@/modules/login/interfaces/LoginResponseInterface";
+
+export class LoginRepository {
+  async authenticate(
+    payload: LoginPayloadInterface
+  ): Promise<LoginResponseInterface> {
+    const response = await post<LoginPayloadInterface>("/login", payload);
+    return response.data;
+  }
+}
