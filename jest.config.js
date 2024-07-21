795cdc0efbe5d4fdfc8e82fa6ee41603795f8998
ErikaNunes <erikanunes0512@gmail.com>
2024-07-21 20:34:33 -0300

    chore: configurando jest e babel para suportar ts

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.vue$": "@vue/vue3-jest",
+    "^.+\\.ts$": "ts-jest",
+    "^.+\\.js$": "babel-jest",
+  },
+  transformIgnorePatterns: ["node_modules/(?!axios/)"],
+  moduleFileExtensions: ["ts", "js", "vue", "json"],
 };
