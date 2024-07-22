eb6a8dfa98709ae5f832f40f716036045ef9de6d
ErikaNunes <erikanunes0512@gmail.com>
2024-07-22 16:47:37 -0300

    feat: redirecionando para dash assim que logar

--- src/modules/login/services/loginService.ts
+++ src/modules/login/services/loginService.ts
@@ -1,8 +1,13 @@
+import { useRouter } from "vue-router";
 import { LoginRepository } from "@/modules/login/repositories/LoginRepository";
 import { LoginPayloadInterface } from "@/modules/login/interfaces/LoginPayloadInterface";
+import { useLoginStore } from "@/modules/login/store";
+import { redirect } from "@/shared/services/RoutingService";
 
 export class LoginService {
   private loginRepository: LoginRepository;
+  private loginStore = useLoginStore();
+  private router = useRouter();
 
   constructor() {
     this.loginRepository = new LoginRepository();
@@ -11,7 +16,10 @@ export class LoginService {
   async signIn(user: LoginPayloadInterface): Promise<void> {
     try {
       const response = await this.loginRepository.authenticate(user);
-      console.log("Token:", response.token);
+      this.loginStore.token = response.token;
+      if (this.loginStore.token !== "") {
+        redirect(this.router, "Dashboard");
+      }
     } catch (error) {
       console.error(error);
     }
