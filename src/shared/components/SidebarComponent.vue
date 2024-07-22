8b4a83fd66d6113940200834386d6df5d7ada676
ErikaNunes <erikanunes0512@gmail.com>
2024-07-22 17:18:06 -0300

    feat: redirecionando para as paginas na sidebar

--- src/shared/components/SidebarComponent.vue
+++ src/shared/components/SidebarComponent.vue
@@ -2,11 +2,11 @@
   <aside>
     <nav class="sidebar bg-light">
       <ul class="list-group">
-        <li class="list-group-item">
+        <li class="list-group-item" @click="handleRedirect('Dashboard')">
           <i class="bi bi-house"></i>
           Dashboard
         </li>
-        <li class="list-group-item">
+        <li class="list-group-item" @click="handleRedirect('Usuários')">
           <i class="bi bi-people"></i>
           Usuários
         </li>
@@ -15,7 +15,15 @@
   </aside>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+import { redirect } from "@/shared/services/RoutingService";
+const router = useRouter();
+
+const handleRedirect = (routeName: string) => {
+  redirect(router, routeName);
+};
+</script>
 
 <style scoped>
 .sidebar {
