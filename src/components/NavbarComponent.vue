990649064fadf30cc9ea00774e026040061e6c91
ErikaNunes <erikanunes0512@gmail.com>
2024-07-21 21:30:19 -0300

    feat: criando componente navbar

---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,12 @@
+<template>
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <span>{{ props.routeName }}</span>
+    </nav>
+  </header>
+</template>
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+const props = defineProps({ routeName: { type: String, requered: true } });
+</script>
