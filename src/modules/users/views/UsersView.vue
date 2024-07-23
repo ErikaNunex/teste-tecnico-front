5fb053e135a02feb35a2c6eb9888055b23924414
ErikaNunes <erikanunes0512@gmail.com>
2024-07-23 14:06:00 -0300

    feat: chamando lista de usuários e paginação

--- src/modules/users/views/UsersView.vue
+++ src/modules/users/views/UsersView.vue
@@ -2,12 +2,31 @@
   <div>
     <UserHeader />
     <UserList />
+    <PaginationList
+      :page="userStore.pagination.page"
+      :perPage="userStore.pagination.perPage"
+      :total="userStore.pagination.total"
+      @pageChange="changePage"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
+import { onMounted } from "vue";
 import UserHeader from "@/modules/users/components/UserHeader.vue";
 import UserList from "@/modules/users/components/UserList.vue";
-</script>
+import { useUserStore } from "../store";
+import { UserService } from "@/modules/users/services/UsersServices";
+import PaginationList from "@/shared/components/PaginationList.vue";
 
+const userService = new UserService();
+const userStore = useUserStore();
+const changePage = (page: number) => {
+  userService.getUsers(page);
+};
+
+onMounted(() => {
+  userService.getUsers(1);
+});
+</script>
 <style scoped></style>
