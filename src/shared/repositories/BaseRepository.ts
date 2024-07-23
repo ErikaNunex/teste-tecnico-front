80761d2f9a22011289bc94adcdd2be3d0c932ff2
ErikaNunes <erikanunes0512@gmail.com>
2024-07-23 13:58:02 -0300

    criando um methodo http get

--- src/shared/repositories/BaseRepository.ts
+++ src/shared/repositories/BaseRepository.ts
@@ -11,3 +11,10 @@ export const post = async <T>(url: string, data: T): Promise<AxiosResponse> => {
   const response = await api.post(url, data);
   return response;
 };
+export const get = async <T>(
+  url: string,
+  params?: Record<string, any>
+): Promise<AxiosResponse<T>> => {
+  const response = await api.get<T>(url, { params });
+  return response;
+};
