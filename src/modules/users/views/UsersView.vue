b98d47fb0e8f9a7ce86e3156194f925591d5aafb
ErikaNunes <erikanunes0512@gmail.com>
2024-07-23 03:58:46 -0300

    feat: importando compenentes de listagem

--- src/modules/users/views/UsersView.vue
+++ src/modules/users/views/UsersView.vue
@@ -1,7 +1,13 @@
 <template>
-  <div class="home">
-    <h1>Users</h1>
+  <div>
+    <UserHeader />
+    <UserList />
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import UserHeader from "@/modules/users/components/UserHeader.vue";
+import UserList from "@/modules/users/components/UserList.vue";
+</script>
+
+<style scoped></style>
