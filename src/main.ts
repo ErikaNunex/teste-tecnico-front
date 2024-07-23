04b9a426626e604bb68e7eecc05a451f2e7b9ee7
ErikaNunes <erikanunes0512@gmail.com>
2024-07-23 17:45:00 -0300

    chore: apex chart

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,13 @@ import App from "./App.vue";
 import router from "./shared/router";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-
+import VueApexCharts from "vue3-apexcharts";
 const app = createApp(App);
 const pinia = createPinia();
 
 app.use(pinia);
 app.use(router);
+app.use(VueApexCharts);
+app.component("VueApexCharts", VueApexCharts);
+
 app.mount("#app");
